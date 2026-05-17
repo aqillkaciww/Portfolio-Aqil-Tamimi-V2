84f8d57342862e8b5f9fe88d32578d912b97b082
Aqilta <aqiltamimi6@gmail.com>
2026-05-17 12:16:13 +0700

    refactor: replace main.css with globals.css, remove unused styles, and move all styling to globals.css

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,4 @@ import Header from './components/Header.vue'
   <main></main>
 </template>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 
-body {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Source Sans Pro', sans-serif;
-}
-
-main {
-  background-color: #f5f1ed;
-}
-</style>
